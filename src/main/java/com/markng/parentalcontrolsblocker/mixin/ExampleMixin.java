c6645ca17e6282693b8fc3d86e7bb58c99dc13ee
CAG2Mark <markng989@gmail.com>
2022-01-07 18:05:47 +0800

    set metadata

--- src/main/java/com/markng/parentalcontrolsblocker/mixin/ExampleMixin.java
+++ src/main/java/com/markng/parentalcontrolsblocker/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package com.markng.parentalcontrolsblocker.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import com.markng.parentalcontrolsblocker.ParentalControlsBlockerMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		ParentalControlsBlockerMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
