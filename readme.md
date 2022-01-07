5734e8940c904e4c3d7d82107557db671bf644bc
CAG2Mark <markng989@gmail.com>
2022-01-07 18:54:30 +0800

    added readme and fixes

---
+++ readme.md
@@ -0,0 +1,9 @@
+# ParentalControlsBlocker
+Fixes the lag spikes in-game due to the Minecraft parental controls. This does **not** affect the client-side hidden players feature, but it *will disable the Microsoft Account based player blocking*.
+
+# Building
+```bash
+git clone https://github.com/CAG2Mark/ParentalControlsBlocker
+cd ParentalControlsBlocker
+./gradlew build
+```
--- src/main/java/com/markng/parentalcontrolsblocker/mixin/HttpRequestBlockerMixin.java
+++ src/main/java/com/markng/parentalcontrolsblocker/mixin/HttpRequestBlockerMixin.java
@@ -1,16 +1,16 @@
 package com.markng.parentalcontrolsblocker.mixin;
 
-import com.markng.parentalcontrolsblocker.ParentalControlsBlockerMod;
-import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(TitleScreen.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		ParentalControlsBlockerMod.LOGGER.info("This line is printed by an example mod mixin!");
+import java.util.UUID;
+
+@Mixin(com.mojang.authlib.yggdrasil.YggdrasilSocialInteractionsService.class)
+public class HttpRequestBlockerMixin {
+	@Inject(at = @At("HEAD"), method = "isBlockedPlayer", cancellable = true, remap = false)
+	private void block_fetch(final UUID uuid, CallbackInfoReturnable<Boolean> cir) {
+		cir.setReturnValue(false);
 	}
 }
